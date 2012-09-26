f65813f2f029cde3d5c9b124a221dae91da3fea5
sam walton <sam@foofrancine.com>
2012-09-26 11:08:23 -0500

    got padding working

--- lib/cal.rb
+++ lib/cal.rb
@@ -22,21 +22,21 @@ class Calendar
 
 #get number of days in a month tabled out
 
-  month_length = {:march => 31, :april => 30, :may => 31, :june => 30, :august => 31, :september => 30, :october => 31, :november => 30, :december => 31, :january => 31, :february => 28}
+  MONTH_LENGTH = {:march => 31, :april => 30, :may => 31, :june => 30, :august => 31, :september => 30, :october => 31, :november => 30, :december => 31, :january => 31, :february => 28}
+  COLUMN_SEPARATOR = "  "
 
-
-def week_start(new_year, new_month)
-    months = %w[march april may june july august september october november december january february]
-    weekdays = %w[Sunday Monday Tuesday Wednesday Thursday Friday Saturday]
-    day_of_month = 1 #day
-    month = months.index(new_month.downcase) + 3
-    year =  month > 12 ? new_year -1 : new_year
-    march_offset = ((month + 1)* 26/10).floor
-    leap_year_offset = (year/4).floor + ((year/100).floor)*6 + (year/400).floor
-    day_of_week = (day_of_month + march_offset + year + leap_year_offset) %7
-
-  return day_of_week
-end
+  def week_start(new_year, new_month)
+      months = %w[march april may june july august september october november december january february]
+      weekdays = %w[Sunday Monday Tuesday Wednesday Thursday Friday Saturday]
+      day_of_month = 1 #day
+      month = months.index(new_month.downcase) + 3
+      year =  month > 12 ? new_year -1 : new_year
+      march_offset = ((month + 1)* 26/10).floor
+      leap_year_offset = (year/4).floor + ((year/100).floor)*6 + (year/400).floor
+      day_of_week = (day_of_month + march_offset + year + leap_year_offset) %7
+  
+    return day_of_week
+  end
 
 #given a month and a year, I can get the column the one starts. 
 #then I can start wrapping days after that. 
@@ -46,7 +46,7 @@ end
 
   #determine if Feb has 28 or 29 days
   def feb?(year)
-    if year.to_f/4 == 0 || year.to_f/400 == 0
+    if year != 1900 && year % 4 == 0 || year % 400 == 0
       29
       else
       28
@@ -54,6 +54,14 @@ end
   end
   
   #figure a way to pad the spaces for where '1' starts
+  def padding
+    week_start(@year, @month)
+    if week_start(@year, @month) == 0
+      6
+      else
+      week_start(@year, @month) - 1
+    end
+  end
 
   #get the delimiters working with "  "
 
--- test/test_cal.rb
+++ test/test_cal.rb
@@ -1,52 +1,72 @@
 require 'test/unit'
-require 'cal.rb'
+require 'cal'
 
 class CalTest < Test::Unit::TestCase
 
   def test_01_dec_01_2041
-    assert_equal(1, week_start(2041, "december"))
+    c = Calendar.new("december", 2041)
+    assert_equal(1, c.week_start(c.year, c.month))
   end
 
   def test_02_feb_01_1865
-    assert_equal(4, week_start(1865, "february"))
+    c = Calendar.new("february", 1865)
+    assert_equal(4, c.week_start(c.year, c.month))
   end
 
   def test_05_jan_01_1998
-    assert_equal(5, week_start(1998, "january"))
+    c = Calendar.new("january", 1998)
+    assert_equal(5, c.week_start(c.year, c.month))
   end
 
   def test_03_may_01_2999
-    assert_equal(6, week_start(2099, "may"))
+    c = Calendar.new("may", 2099)
+    assert_equal(6, c.week_start(c.year, c.month))
   end
 
   def test_04_Aug_01_1999
-    assert_equal(1, week_start(1999, "august"))
+    c = Calendar.new("august", 1999)
+    assert_equal(1, c.week_start(c.year, c.month))
   end
 
   def test_06_Sep_01_1888
-    assert_equal(0, week_start(1888, "september"))
+    c = Calendar.new("september", 1888)
+    assert_equal(0, c.week_start(c.year, c.month))
   end
 
   def test_07_Mar_01_1889
-    assert_equal(6, week_start(1889, "march"))
+    c = Calendar.new("march", 1889)
+    assert_equal(6, c.week_start(c.year, c.month))
   end
   
   def test_08_2013_should_return_28
-    assert_equal(28, feb?(2013))
+    c = Calendar.new("may", 2013)
+    assert_equal(28, c.feb?(c.year))
   end
 
   def test_09_2012_should_return_29
-    assert_equal(29, feb?(2012))
+    c = Calendar.new("may", 2012)
+    assert_equal(29, c.feb?(c.year))
   end
 
   def test_10_1900_should_return_28
-    assert_equal(28, feb?(1900))
+    c = Calendar.new("may", 1900)
+    assert_equal(28, c.feb?(c.year))
   end
 
   def test_11_2000_should_return_29
-    assert_equal(29, feb?(2000))
+    c = Calendar.new("may", 2000)
+    assert_equal(29, c.feb?(c.year))
   end
 
+  def test_12_Monday_should_pad_1
+    c = Calendar.new("may", 2000)
+    assert_equal(1, c.padding)
+  end
+
+  def test_12_Saturday_should_pad_6
+    c = Calendar.new("september", 1888)
+    assert_equal(6, c.padding)
+  end
 
 
 end
